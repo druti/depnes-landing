5a3c25e0853508b096786eeadc2e4d40bf6856d1
Andres Carbo <carbindustries@gmail.com>
2017-01-21 21:11:05 -0500

    Added logo and other images.

--- server/server.js
+++ server/server.js
@@ -2,13 +2,16 @@ var http = require('http');
 var root = require('./routes/root');
 var css = require('./routes/css');
 var js = require('./routes/js');
+var img = require('./routes/img');
 var hostname = '127.0.0.1';
 var port = process.env.PORT || 3333;
 
 var server = http.createServer(function (req, res) {
+  console.log('Loading resource... ', req.url);
   root(req, res);
   css(req, res);
   js(req, res);
+  img(req,res);
 });
 
 server.listen(port, function () {
